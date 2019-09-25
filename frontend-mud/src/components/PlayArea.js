5c7d91c5537f92075fad171c812ecc174481f0a2
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-09-24 17:30:43 -0700

    remove unneccessary console logs

--- frontend-mud/src/components/PlayArea.js
+++ frontend-mud/src/components/PlayArea.js
@@ -1,19 +1,17 @@
-import React, {useState} from 'react'
-import {useFetch} from "../utils/useFetch"
+import React from "react";
+import { useFetch } from "../utils/useFetch";
 // holds the room state and number of tiles in the room.
 // valid directions will render a new room
 
-const PlayArea = () =>{
-
-const { loading, data: room, error } = useFetch(
+const PlayArea = () => {
+  //    { bool,    array,      bool  }
+  const { loading, data: room, error } = useFetch(
     "https://lambda-mud-test.herokuapp.com/api/adv/rooms"
-  )
-// [ { name: ocean, position: 123143 } ]
-console.log(room, loading,error)
-    return (
-        <div>
-            
-        </div>
-    )
-}
-export default PlayArea
+  );
+
+  // [ { name: ocean, position: 123143 } ]
+  console.log(room, loading, error);
+
+  return <div></div>;
+};
+export default PlayArea;
--- frontend-mud/src/utils/useFetch.js
+++ frontend-mud/src/utils/useFetch.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react" 
+import React from "react" 
 
 export const fetchReducer = (state, action) => {
     if (action.type === 'get') {
